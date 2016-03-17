d7d46cbc368b922ccc39bd9ed7f53eb8712f1050
quentinleduc <quentinleduc03@gmail.com>
2016-03-17 07:55:17 +0000

    Remove c9 temp files

--- .~c9_invoke_IYZ3N7.py
+++
@@ -1,8 +0,0 @@
-from libs.bottle import route, template, run
-
-@route('/')
-def index(name):
-    return template("<b>Hello {{name}}!" name=name
-    return template("<b>Hello {{name}}!", name=name)
-
-run(host='0.0.0.0', port=8080)
--- .~c9_invoke_WKG8u.py
+++
@@ -1,8 +0,0 @@
-from libs.bottle import route, template, run
-
-@route('/')
-def index(name):
-    
-    return template("<b>Hello {{name}}!", name=name)
-run(host='localho', port=8081)
-run(host='0.0.0.0', port=8080)
